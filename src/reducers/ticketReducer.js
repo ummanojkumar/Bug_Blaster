e60d2ba6b3aab9ee8b7a940c96ee7887be3d4c10
ummanojkumar <ummanojkumar92@gmail.com>
2025-03-15 23:02:04 +0530

    Empty Edited contenct when click delete button

--- src/reducers/ticketReducer.js
+++ src/reducers/ticketReducer.js
@@ -6,9 +6,13 @@ export default function ticketReducer(state, action) {
         case 'ADD_TICKET':
             return {...state, tickets: [...state.tickets, action.payload]};
         case 'UPDATE_TICKET':
-            return {...state, tickets: state.tickets.map(ticket => ticket.id === action.payload.id ? action.payload : ticket )}
+            return {...state, tickets: state.tickets.map(ticket => ticket.id === action.payload.id ? action.payload : ticket ), editingTicket:null}
         case 'DELETE_TICKET':
-            return {...state, tickets: state.tickets.filter(ticket => ticket.id !== action.payload.id )}
+            if(state.editingTicket && state.editingTicket.id === action.payload.id) {
+                return {...state, tickets: state.tickets.filter(ticket => ticket.id !== action.payload.id ), editingTicket : null}
+            } else {
+                return {...state, tickets: state.tickets.filter(ticket => ticket.id !== action.payload.id )}
+            }
         case 'SET_EDITING_TICKET':
             return {...state, editingTicket:action.payload};
         case 'CLEAR_EDITING_TICKET':
