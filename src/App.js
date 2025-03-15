48bc5e42c1df69f75f154109300e778384143fa3
ummanojkumar <ummanojkumar92@gmail.com>
2025-03-15 18:19:52 +0530

    reducer ADD_Ticket

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
 import './styles.css'
+import { useReducer } from 'react';
 import TicketForm from './components/TicketForm';
+import ticketReducer from './reducers/ticketReducer';
 
 function App() {
+
+  const intialState = {tickets : []}
+
+  const[state, dispatch] = useReducer(ticketReducer, intialState);
+
   return (
     <div className="App">
       <div className='container'>
         <h1>Bug Blaster</h1>
-        <TicketForm></TicketForm>
+        <TicketForm dispatch={dispatch}></TicketForm>
+        {
+          console.log({state})
+          
+        }
 
       </div>
     </div>
--- src/components/TicketForm.js
+++ src/components/TicketForm.js
@@ -1,6 +1,6 @@
 import React, {useState} from "react";
 
-export default function TicketForm() {
+export default function TicketForm({dispatch}) {
 
     const [title, setTitle] = useState('');
     const [description, setDescription] = useState('');
@@ -27,6 +27,12 @@ export default function TicketForm() {
             description,
             priority,
         };
+
+        dispatch({
+            type : 'ADD_TICKET',
+            payload : ticketData
+        })
+
 
         clearForm();
     }
